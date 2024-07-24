def36bbc27398ae6be819859e206b0ca040949fb
Travisdv <rockchristian546@gmail.com>
2024-07-24 07:59:36 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '2349066380818' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=vAsBRJpY#C-giwVgc9U7cpyjwT66QAUsg61-T3fWU8c-UGYStcyo' ,
+SESSION_ID: process.env.SESSION_ID || '' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
